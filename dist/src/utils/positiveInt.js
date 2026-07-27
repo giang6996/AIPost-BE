c3f6087fafff193a5e51e3e19bf9a5b3f42dba6e
giang6996 <gmod6996@gmail.com>
2026-07-28 00:12:23 +0700

    Ignore dist due to previous commit modified gitignore

--- dist/src/utils/positiveInt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parsePositiveInt = parsePositiveInt;
-function parsePositiveInt(value) {
-    const parsed = Number(value);
-    return Number.isInteger(parsed) && parsed > 0 ? parsed : null;
-}
