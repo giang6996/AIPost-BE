d5551c1c91bc982fa4e405b2c6aa0f4d9e726aac
giang6996 <giangnguyencanh69@gmail.com>
2026-08-12 04:47:24 +0700

    fix: force vitest to skip complied code for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    exclude: [
+      '**/dist/**'
+    ],
     environment: 'node',
     globals: true,
     setupFiles: ['./src/tests/setup.ts'],
