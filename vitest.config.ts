d88255b45bfccd5c2ceb02df20bb02151d0cceac
giang6996 <giangnguyencanh69@gmail.com>
2026-08-12 04:59:28 +0700

    fix: modifed include/exclude parameter for vitest to ensure correct testing behavior

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+
+    dir: './src/tests', 
+    
+    // 2. Only match test files located inside the test folder
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], 
+    
+    // 3. Explicitly block the dist folder and other build artifacts
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.{idea,git,cache,output,temp}/**'
+    ],
+
     environment: 'node',
     globals: true,
     setupFiles: ['./src/tests/setup.ts'],
